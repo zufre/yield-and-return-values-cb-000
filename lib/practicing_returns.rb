d989876e264edc0409be56294ed990f01eaaa8fc
Cedric David <cedric.david@btopenworld.com>
2018-09-02 05:58:26 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -2,13 +2,10 @@ require 'pry'
 
 def hello(array)
   i = 0
-  collection=[]
   while i < array.length
-
-    collection << yield(array[i])
+    yield(array[i])
     i += 1
   end
-  collection
 end
 
 
