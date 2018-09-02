1ab16c4f66a250c89ebc7410b4c6f757d9f28582
Cedric David <cedric.david@btopenworld.com>
2018-09-02 06:07:36 +0000

    automatically backed up by learn

--- lib/practicing_returns.rb
+++ lib/practicing_returns.rb
@@ -1,3 +1,5 @@
+
+
 def hello(array)
   i = 0
   collection = []
@@ -7,4 +9,4 @@ def hello(array)
   end
   collection
 end
-hello(["Tim", "Tom", "Jim"]) { |name| puts "Hi, #{name}" }
+hello(["Tim", "Tom", "Jim"]) { |name|  "Hi, #{name}" }
